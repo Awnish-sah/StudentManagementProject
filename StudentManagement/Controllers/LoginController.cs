7a92175c04e2268d97a1f663da3a3aeecf25e479
Avanish Sah <sahawnish2014@gmail.com>
2021-08-03 19:43:33 +0545

    My First Commit

---
+++ StudentManagement/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using OnlineShoppingStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class LoginController : Controller
+    {
+        student_dbEntities db = new student_dbEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user objchk)
+        {
+            if(ModelState.IsValid)
+            {
+                using(student_dbEntities db = new student_dbEntities())
+                {
+                    var obj = db.users.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["Username"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The Username or Password Incorrect");
+
+                    }
+
+                }
+
+            }
+            return View(objchk);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
